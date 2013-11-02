8f9e3133cc5f61c208557b3acecd8b65582f08c1
tonoman3g <tonoman3g@gmail.com>
2013-11-02 21:00:23 +0700

    commit

--- workspace/JalinSuara/src/com/jalinsuara/android/BaseActivity.java
+++ workspace/JalinSuara/src/com/jalinsuara/android/BaseActivity.java
@@ -1,7 +1,5 @@
 package com.jalinsuara.android;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- workspace/JalinSuara/src/com/jalinsuara/android/news/NewsAdapter.java
+++ workspace/JalinSuara/src/com/jalinsuara/android/news/NewsAdapter.java
@@ -22,11 +22,14 @@ public class NewsAdapter extends BaseAdapter {
 	private ArrayList<News> mList;
 	private ImageLoader mImageLoader;
 	private Context mContext;
+	private LayoutInflater inflater;
 
 	public NewsAdapter(Context context, ArrayList<News> list) {
 		mList = list;
 		mImageLoader = new ImageLoader(context);
 		mContext = context;
+		inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
 	@Override
@@ -48,9 +51,6 @@ public class NewsAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		News object = mList.get(position);
 		if (convertView == null) {
-			LayoutInflater inflater = (LayoutInflater) parent.getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
 			convertView = inflater.inflate(R.layout.list_item_news, null);
 
 		}
@@ -59,12 +59,10 @@ public class NewsAdapter extends BaseAdapter {
 				.findViewById(R.id.list_item_news_title_textview));
 		TextView descriptionTextview = ((TextView) convertView
 				.findViewById(R.id.list_item_news_content_textview));
-		
+
 		convertView.setTag(object.getId());
 		titleTextview.setText(object.getTitle());
 		descriptionTextview.setText(Html.fromHtml(object.getDescription()));
-		
-		
 
 		return convertView;
 	}
