f6bd62247fd9fa0fa8ad20d7f400199c6fbd8bf1
tonoman3g <tonoman3g@gmail.com>
2013-11-03 17:21:36 +0700

    fix error

--- workspace/JalinSuara/src/com/jalinsuara/android/home/SearchableActivity.java
+++ workspace/JalinSuara/src/com/jalinsuara/android/home/SearchableActivity.java
@@ -1,7 +1,5 @@
 package com.jalinsuara.android.home;
 
-
-
 import com.jalinsuara.android.R;
 import android.app.Activity;
 import android.app.DownloadManager.Query;
@@ -12,39 +10,43 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class SearchableActivity extends Activity{
+public class SearchableActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.search);
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.search);
 
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      doMySearch(query);
-	    }
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			doMySearch(query);
+		}
 	}
-	
-	public void doMySearch(String query){
-		
+
+	public void doMySearch(String query) {
+
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-	    return true;
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-		case R.id.search:
-	        onSearchRequested();
-	        return true;
-	    default:
-	        return false;
-	    
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			onSearchRequested();
+			return true;
+		default:
+
+			return false;
+		}
+
 	}
-	
+
 }
